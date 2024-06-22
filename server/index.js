85e181f78349b4ca5e18698c37cd8a93d9dad9be
abhikunu007 <ak0879918@gmail.com>
2024-06-22 23:22:35 +0530

    add cors middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
 const {connectToMongoDB} = require("./mongoose");
+const cors = require('cors');
 
 const URL = require("./models/url");
 
@@ -11,6 +12,8 @@ connectToMongoDB("mongodb://0.0.0.0:27017/short-url")
 .then(
     console.log("MongoDB Connected!")
 )
+
+app.use(cors());
 
 app.use(express.json());
 
