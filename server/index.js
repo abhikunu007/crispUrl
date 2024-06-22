fac140c6d888991b032115f6f5a19ac564b177b3
abhikunu007 <ak0879918@gmail.com>
2024-06-22 17:27:40 +0530

    complete backend

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const urlRoute = require("./routes/url");
+const {connectToMongoDB} = require("./mongoose");
+
+const URL = require("./models/url");
+
+const app = express();
+const port = 8000;
+
+connectToMongoDB("mongodb://0.0.0.0:27017/short-url")
+.then(
+    console.log("MongoDB Connected!")
+)
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+
+
+app.listen(port, ()=> console.log(`Server Started on Port: ${port}`));
